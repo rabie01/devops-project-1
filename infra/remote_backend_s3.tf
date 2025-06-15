8eb73a36f51fcb3eb0b9c4861c5668cfda34f75d
rabie01 <rabeea2100@yahoo.com>
2025-06-15 11:59:18 -0500

    adjust the vars and s3 bucket

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-123456"
-    key    = "devops-project-1/terraform.tfstate"
-    region = "eu-central-1"
+    bucket = "mybuckett21000"
+    key    = "terraform/devops-project-1-app/terraform.tfstate"
+    region = "us-east-1"
   }
 }
